681ba7b9ab8c9745214097cc509fd2450c28b0b7
Devin Rosenbauer <devin@identityworksllc.com>
2023-12-07 14:32:45 -0500

    Update install script to respect cert

--- iiq-build/database-setup.mssql.sh
+++ iiq-build/database-setup.mssql.sh
@@ -8,7 +8,8 @@
 #   SKIP_DEMO_IMPORT
 
 #check if database schema is already there
-export DB_SCHEMA_VERSION=$(sqlcmd -U ${MSSQL_SA_USER} -P ${MSSQL_SA_PASSWORD} -S ${MSSQL_HOST} -d identityiq -Q "select schema_version from identityiq.spt_database_version;")
+export DB_SCHEMA_VERSION=$(sqlcmd -C -N o -U ${MSSQL_SA_USER} -P ${MSSQL_SA_PASSWORD} -S ${MSSQL_HOST} -d identityiq -Q "select schema_version from 
+identityiq.spt_database_version;")
 if [ -z "$DB_SCHEMA_VERSION" ]
 then
 	echo "=> No schema present, creating IIQ schema in MSSQL DB" 
@@ -18,23 +19,25 @@ then
 	if [[ -e /opt/tomcat/webapps/identityiq/WEB-INF/database/create_identityiq_tables.sqlserver ]]; then
 		sed -ri -e "s/PASSWORD='identityiq'/PASSWORD='${MSSQL_PASS}'/" /opt/tomcat/webapps/identityiq/WEB-INF/database/create_identityiq_tables.sqlserver
 		sed -ri -e "s/PASSWORD='identityiqPlugin'/PASSWORD='${MSSQL_PASS}'/" /opt/tomcat/webapps/identityiq/WEB-INF/database/create_identityiq_tables.sqlserver
-		sqlcmd -U ${MSSQL_SA_USER} -P ${MSSQL_SA_PASSWORD} -S ${MSSQL_HOST} -b -i /opt/tomcat/webapps/identityiq/WEB-INF/database/create_identityiq_tables.sqlserver
+		sqlcmd -C -N o -U ${MSSQL_SA_USER} -P ${MSSQL_SA_PASSWORD} -S ${MSSQL_HOST} -b -i 
+/opt/tomcat/webapps/identityiq/WEB-INF/database/create_identityiq_tables.sqlserver
 	else
 		sed -ri -e "s/PASSWORD='identityiq'/PASSWORD='${MSSQL_PASS}'/" /opt/tomcat/webapps/identityiq/WEB-INF/database/create_identityiq_tables-${IIQ_VERSION}.sqlserver
 		sed -ri -e "s/PASSWORD='identityiqPlugin'/PASSWORD='${MSSQL_PASS}'/" /opt/tomcat/webapps/identityiq/WEB-INF/database/create_identityiq_tables-${IIQ_VERSION}.sqlserver
-		sqlcmd -U ${MSSQL_SA_USER} -P ${MSSQL_SA_PASSWORD} -S ${MSSQL_HOST} -b -i /opt/tomcat/webapps/identityiq/WEB-INF/database/create_identityiq_tables-${IIQ_VERSION}.sqlserver
+		sqlcmd -C -N o -U ${MSSQL_SA_USER} -P ${MSSQL_SA_PASSWORD} -S ${MSSQL_HOST} -b -i 
+/opt/tomcat/webapps/identityiq/WEB-INF/database/create_identityiq_tables-${IIQ_VERSION}.sqlserver
 	fi
 	
 	echo "=> Done creating database, checking for upgrades..."
 	cd /opt/tomcat/webapps/identityiq/WEB-INF/database/
 	if [[ -e upgrade_identityiq_tables.sqlserver ]]; then
 		echo "=> Installing custom upgrade script"
-		sqlcmd -U ${MSSQL_SA_USER} -P ${MSSQL_SA_PASSWORD} -S ${MSSQL_HOST} -i upgrade_identityiq_tables.sqlserver
+		sqlcmd -C -N o -U ${MSSQL_SA_USER} -P ${MSSQL_SA_PASSWORD} -S ${MSSQL_HOST} -i upgrade_identityiq_tables.sqlserver
 	else
 		for upgrade in `ls upgrade_identityiq_tables-${IIQ_VERSION}*.sqlserver | sort`
 		do
 			echo "=> Installing upgrade $upgrade"
-			sqlcmd -U ${MSSQL_SA_USER} -P ${MSSQL_SA_PASSWORD} -S ${MSSQL_HOST} -i $upgrade
+			sqlcmd -C -N o -U ${MSSQL_SA_USER} -P ${MSSQL_SA_PASSWORD} -S ${MSSQL_HOST} -i $upgrade
 		done
 	fi
 	
